569e4a1b53cf3be438a40cf070f5283da9fbea8a
Tirdad <abbasi.tirdad@gmail.com>
2021-07-28 23:52:28 +0430

    Create and Input development

---
+++ src/react/src/components/Input.js
@@ -0,0 +1,10 @@
+import { Input } from 'antd';
+
+
+const Text = () => {
+    return(
+        <Input placeholder="Basic usage" />
+    )
+}
+
+export default Text;
--- src/react/src/pages/Create.js
+++ src/react/src/pages/Create.js
@@ -1,12 +1,54 @@
 import DefaultLayout from '../layouts/DefaultLayout'
+// import Input from "../components/Input";
+
+import React, { lazy, useEffect, useState } from 'react';
+// import shortid from 'shortid';
 
 
-function Create() {
-  return (
-      <DefaultLayout>
-          <h1>Create</h1>
-      </DefaultLayout>
-  );
+const importView = subreddit =>
+    lazy(() =>
+        import(`../components/${subreddit}`).catch(() =>
+            import(`./404`)
+        )
+    );
+
+
+const searchSubreddit = async query =>
+    fetch(
+        `http://localhost:8000/fields.json`
+    ).then(_ => _.json());
+
+function Create({ subredditsToShow }) {
+    const [views, setViews] = useState([]);
+
+    const extractData = response =>
+        response.data.children.map(({ data }) => data);
+
+    useEffect(() => {
+        async function loadViews() {
+            const subredditsToShow = await searchSubreddit(
+                'react hooks'
+            ).then(extractData);
+            const componentPromises = subredditsToShow.map(
+                async data => {
+                    const View = await importView(data.subreddit);
+                    return (
+                        <View key='1' {...data} />
+                    );
+                });
+
+            Promise.all(componentPromises).then(setViews);
+        }
+
+        loadViews();
+    }, [subredditsToShow]);
+
+
+    return (
+        <React.Suspense fallback='Loading views...'>
+            <div className='container'>{views}</div>
+        </React.Suspense>
+    );
 }
 
 export default Create;
