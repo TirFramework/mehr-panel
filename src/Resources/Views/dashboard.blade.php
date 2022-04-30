9218257b4404d535be06130a95d4c6ff624f169b
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2022-04-30 15:46:23 +0300

    laravel mix added on css and js

--- src/Resources/Views/dashboard.blade.php
+++ src/Resources/Views/dashboard.blade.php
@@ -6,14 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> --}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{asset('/admin-assets/css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('/admin-assets/css/tailwindcss.css')}}">
+    <link rel="stylesheet" href="{{mix('/admin-assets/css/app.css')}}">
+    <link rel="stylesheet" href="{{mix('/admin-assets/css/tailwindcss.css')}}">
     <link rel="stylesheet" href="{{asset('/admin-assets/css/custom.css')}}">
 </head>
 <body>
-    <div id="root"></div>
+<div id="root"></div>
 
-    <script src="{{asset('/admin-assets/js/index.js')}}"></script>
+<script src="{{mix('/admin-assets/js/index.js')}}"></script>
 
 </body>
 </html>
