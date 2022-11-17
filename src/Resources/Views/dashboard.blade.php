e664f039ef558ac50d92e8c0af2d4e4b0e955315
mehrdad <javadabbasi72@gmail.com>
2022-11-17 23:09:29 +0330

    remove app from admin

--- src/Resources/Views/dashboard.blade.php
+++ src/Resources/Views/dashboard.blade.php
@@ -1,19 +1,21 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     {{-- <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> --}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{mix('/admin-assets/css/app.css')}}">
     <link rel="stylesheet" href="{{mix('/admin-assets/css/tailwindcss.css')}}">
     <link rel="stylesheet" href="{{asset('/admin-assets/css/custom.css')}}">
 </head>
-<body>
-<div id="root"></div>
 
-<script src="{{mix('/admin-assets/js/index.js')}}"></script>
+<body>
+    <div id="root"></div>
+
+    <script src="{{mix('/admin-assets/js/index.js')}}"></script>
 
 </body>
+
 </html>
