13e8673fbee5d5c32f0c6941c9c9e08524cb03f2
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2022-12-01 11:05:44 +0100

    update datepicker

--- src/react/src/components/DatePicker.js
+++ src/react/src/components/DatePicker.js
@@ -4,29 +4,35 @@ import {separationRules} from "../lib/helpers";
 
 const Text = (props) => {
 
-const rules = separationRules({
-    pageType: props.pageType,
-    rules: props.rules,
-    creationRules: props.creationRules,
-    updateRules: props.updateRules,
-});
+    const rules = separationRules({
+        pageType: props.pageType,
+        rules: props.rules,
+        creationRules: props.creationRules,
+        updateRules: props.updateRules,
+    });
 
-  return (
-    <>
-      <Form.Item
-        label={props.label}
-        name={props.name}
-        initialValue={props.value}
-        rules={rules}
-      >
-        <DatePicker
-            placeholder={props.options.placeholder}
-            disabled={props.readonly}
-            className={`${props.readonly && "readOnly"} w-full`}
-        />
-      </Form.Item>
-    </>
-  );
+    const dateFormat = 'YYYY-MM-DD';
+
+
+    return (
+        <>
+            <Form.Item
+                label={props.label}
+                name={props.name}
+                initialValue={props.value}
+                rules={rules}
+            >
+                <DatePicker
+                    format={dateFormat}
+                    placeholder={props.options.placeholder}
+                    disabled={props.readonly}
+                    className={`${props.readonly && "readOnly"} w-full`}
+                    style={{ width: '100%' }}
+                />
+            </Form.Item>
+
+        </>
+    );
 };
 
 export default Text;
