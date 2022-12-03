33cf5c4ffc63f3b0570dede13adeb1347f9758e7
mehrdad <javadabbasi72@gmail.com>
2022-12-03 18:39:22 +0330

    bug fix

--- src/react/src/components/Checkbox.js
+++ src/react/src/components/Checkbox.js
@@ -1,21 +1,31 @@
-
 import { Form, Checkbox } from "antd";
+import { separationRules } from "../lib/helpers";
 
-const Text = (data) => {
+const Text = (props) => {
+  const rules = separationRules({
+    pageType: props.pageType,
+    rules: props.rules,
+    creationRules: props.creationRules,
+    updateRules: props.updateRules,
+  });
   return (
     <>
       <Form.Item
-        label={data.label}
-        name={data.name}
-        initialValue={data.val}
-        rules={[
-          {
-            required: true,
-            // message: "Please input your username!",
-          },
-        ]}
+        label={props.display}
+        name={props.name}
+        initialValue={props.value}
+        rules={rules}
+        labelCol={{
+          flex: "none",
+        }}
+        wrapperCol={{
+          flex: "auto",
+        }}
       >
-        <Checkbox />
+        <Checkbox
+          disabled={props.readonly}
+          className={props.readonly && "readOnly"}
+        />
       </Form.Item>
     </>
   );
