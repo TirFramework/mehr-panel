620310f585c89057686aaff7bd766d2e56cf4cec
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2023-01-04 14:21:54 +0100

    datepicker field updated

--- src/react/src/components/DatePicker.js
+++ src/react/src/components/DatePicker.js
@@ -1,43 +1,46 @@
 import { Form, DatePicker } from "antd";
 import { separationRules } from "../lib/helpers";
-import dayjs from "dayjs";
-import customParseFormat from "dayjs/plugin/customParseFormat";
+import moment from "moment"
 
-dayjs.extend(customParseFormat);
 
 const Text = (props) => {
-  const dateFormat = props.options?.dateFormat
-    ? props.options.dateFormat
-    : "YYYY-MM-DD";
+    const dateFormat = props.options.dateFormat
+        ? props.options.dateFormat
+        : "YYYY-MM-DD";
 
-  const rules = separationRules({
-    pageType: props.pageType,
-    rules: props.rules,
-    creationRules: props.creationRules,
-    updateRules: props.updateRules,
-  });
+    const picker = props.options.picker
+        ? props.options.picker
+        : "day";
 
-  return (
-    <>
-      <Form.Item
-        label={props.display}
-        name={props.name}
-        initialValue={
-          props.value ? dayjs(props.value, dateFormat) : props.value
-        }
-        rules={rules}
-        format={dateFormat}
-      >
-        <DatePicker
-          format={dateFormat}
-          placeholder={props.options.placeholder}
-          disabled={props.readonly}
-          className={`${props.readonly && "readOnly"} w-full`}
-          style={{ width: "100%" }}
-        />
-      </Form.Item>
-    </>
-  );
+    const rules = separationRules({
+        pageType: props.pageType,
+        rules: props.rules,
+        creationRules: props.creationRules,
+        updateRules: props.updateRules,
+    });
+
+    return (
+        <>
+            <Form.Item
+                label={props.display}
+                name={props.name}
+                initialValue={
+                    props.value ? moment.utc(props.value, dateFormat) : props.value
+                }
+                rules={rules}
+                format={dateFormat}
+            >
+                <DatePicker
+                    format={dateFormat}
+                    placeholder={props.options.placeholder}
+                    disabled={props.readonly}
+                    picker={picker}
+                    className={`${props.readonly && "readOnly"} w-full`}
+                    style={{ width: "100%" }}
+                />
+            </Form.Item>
+        </>
+    );
 };
 
 export default Text;
