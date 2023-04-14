47767a717270eabce59813b9c36787db70e0d9bb
mehrdad <javadabbasi72@gmail.com>
2023-04-14 15:25:41 +0430

    develop

--- src/react/src/components/DatePicker.js
+++ src/react/src/components/DatePicker.js
@@ -1,16 +1,31 @@
 import { Form, DatePicker } from "antd";
 import { separationRules } from "../lib/helpers";
-import moment from "moment"
+import moment from "moment";
 
+const Date = (props) => {
+  return (
+    <DatePicker
+      format={props.dateFormat}
+      placeholder={props.options.placeholder}
+      disabled={props.readonly}
+      value={moment(props.value, props.dateFormat)}
+      picker={props.options.picker || ""}
+      className={`${props.readonly && "readOnly"} w-full`}
+      style={{ width: "100%" }}
+      onChange={(e, v) => {
+        props.onChange(v);
+      }}
+    />
+  );
+};
 
 const Text = (props) => {
   const dateFormat = props.options.dateFormat
     ? props.options.dateFormat
     : "YYYY-MM-DD";
-
-  const picker = props.options.picker
-    ? props.options.picker
-    : "day";
+  // const stillUtc = moment.utc(props.value);
+  // // const local = moment(stillUtc).local();
+  // console.log("🚀 ~ file: DatePicker.js:22 ~ Text ~ local:", stillUtc);
 
   const rules = separationRules({
     pageType: props.pageType,
@@ -24,20 +39,11 @@ const Text = (props) => {
       <Form.Item
         label={props.display}
         name={props.name}
-        initialValue={
-          props.value ? moment.utc(props.value, dateFormat) : props.value
-        }
+        initialValue={props.value}
         rules={rules}
         format={dateFormat}
       >
-        <DatePicker
-          format={dateFormat}
-          placeholder={props.options.placeholder}
-          disabled={props.readonly}
-          picker={picker}
-          className={`${props.readonly && "readOnly"} w-full`}
-          style={{ width: "100%" }}
-        />
+        <Date dateFormat={dateFormat} {...props} />
       </Form.Item>
     </>
   );
