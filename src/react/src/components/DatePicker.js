3e089f7958b22cac5a328074804b542a26998c8e
mehrdad <javadabbasi72@gmail.com>
2023-07-22 17:04:13 +0330

    date picker fix

--- src/react/src/components/DatePicker.js
+++ src/react/src/components/DatePicker.js
@@ -1,16 +1,26 @@
 import { Form, DatePicker } from "antd";
 import { separationRules } from "../lib/helpers";
-import moment from "moment"
+import moment from "moment";
 
+const CustomDatePicker = ({ format, onChange, value, ...props }) => {
+  return (
+    <DatePicker
+      {...props}
+      format={format}
+      onChange={(data) => {
+        onChange(moment(data).format(format));
+      }}
+      value={value ? moment.utc(value, format) : null}
+    />
+  );
+};
 
 const Text = (props) => {
   const dateFormat = props.options.dateFormat
     ? props.options.dateFormat
     : "YYYY-MM-DD";
 
-  const picker = props.options.picker
-    ? props.options.picker
-    : "date";
+  const picker = props.options.picker ? props.options.picker : "date";
 
   const rules = separationRules({
     pageType: props.pageType,
@@ -24,13 +34,10 @@ const Text = (props) => {
       <Form.Item
         label={props.display}
         name={props.name}
-        initialValue={
-          props.value ? moment.utc(props.value, dateFormat) : props.value
-        }
+        initialValue={props.value}
         rules={rules}
-        format={dateFormat}
       >
-        <DatePicker
+        <CustomDatePicker
           format={dateFormat}
           placeholder={props.options.placeholder}
           disabled={props.readonly}
