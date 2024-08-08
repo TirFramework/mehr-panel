bd5ddbbcc7faf22e407ff726f91dcc09e6efde68
mehrdad <javadabbasi72@gmail.com>
2024-08-08 16:08:12 +0330

    change title

--- src/Resources/Views/dashboard.blade.php
+++ src/Resources/Views/dashboard.blade.php
@@ -6,7 +6,7 @@
     {{-- <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> --}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Admin Panel</title>
     <link rel="stylesheet" href="{{mix('/admin-assets/css/custom.css')}}" />
     <link rel="stylesheet" href="{{mix('/admin-assets/css/tailwindcss.css')}}" />
 </head>
