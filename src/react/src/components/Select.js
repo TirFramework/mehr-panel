ec66a0ba4b27c2bba027054859f1b194d8e44625
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2025-08-22 11:23:48 +0200

    console log removed from select.js

--- src/react/src/components/Select.js
+++ src/react/src/components/Select.js
@@ -49,37 +49,33 @@ const SelcetIndex = (props) => {
   });
 
   if (props.readonly) {
-
     if (props.value) {
-        if (typeof props.value === "object") {
-            return (
-                <>
-                    {props.hideLable ?? <div>{props.display}</div>}
-                    <div>
-                        {props.value.map((i) => {
-                                console.log(`this is the ${props.dataSet[i]}`);
-                                if (!props.dataSet[i]) {
-                                        return (<Tag>{i} not found</Tag>);
-                                }
-                                        return (<Tag>{props.dataSet[i]}</Tag>)
-                                }
-                        )}
-                    </div>
-                </>
-            );
-        } else {
-            return (
-                <>
-                    {props.hideLable ?? <div>{props.display}</div>}
-                    <div>
-                        <Tag>{props.dataSet[props.value]}</Tag>
-                    </div>
-                </>
-            );
-        }
+      if (typeof props.value === "object") {
+        return (
+          <>
+            {props.hideLable ?? <div>{props.display}</div>}
+            <div>
+              {props.value.map((i) => {
+                if (!props.dataSet[i]) {
+                  return <Tag>{i} not found</Tag>;
+                }
+                return <Tag>{props.dataSet[i]}</Tag>;
+              })}
+            </div>
+          </>
+        );
+      } else {
+        return (
+          <>
+            {props.hideLable ?? <div>{props.display}</div>}
+            <div>
+              <Tag>{props.dataSet[props.value]}</Tag>
+            </div>
+          </>
+        );
+      }
     }
     return;
-
   }
 
   return (
