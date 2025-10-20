4555db24f66b9070fe6dcd151aacb5f86e608f8d
Mehrdad Abasi <javadabbasi72@gmail.com>
2025-10-20 14:18:20 +0330

    Update version to 12.3 across package.json and config.js files

--- src/react/src/constants/config.js
+++ src/react/src/constants/config.js
@@ -13,7 +13,7 @@ const Config = {
     "/storage",
   tinyemcApiKey: process.env.MIX_APP_TINYEMC || process.env.REACT_APP_TINYEMC,
   perfix: panelName,
-  panelVersion: "12.2",
+  panelVersion: "12.3",
   interactionCharacter:
     process.env.MIX_APP_INTERACTION_CHARACTER ||
     process.env.REACT_APP_INTERACTION_CHARACTER ||
